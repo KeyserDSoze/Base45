77322914f926fd230ce0e702346bec42aff86f31
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2021-04-01 23:02:53 +0200

    v. 1.0.3 - check the length and the values in decrypt. code optimized.

--- Base45.UnitTest/Base45Test.cs
+++ Base45.UnitTest/Base45Test.cs
@@ -15,6 +15,33 @@ namespace Base45.UnitTest
         [Fact]
         public void Large()
             => TestPhrase("Hello!! I'm a galaxy guardian and I want to remove the last feature in my backlog because I want to defeat the goliath in my heart on my earth through the hearth");
+        [Fact]
+        public void WrongLength()
+        {
+            try
+            {
+                "6BFOXNQG2OAJVUN-8R:*L546/XKPWOSFB60JO DJS4QPR-%2QT45Y7AVD571VX3 43$R71YL423123LF60H9I%5LKB3DJ1OTM7BZGL%0KSVBGZIUSQT35CL6U+QQHPX*OJ+PVC6-S48346-1-P2MED2/D9KD $T2E22R59/9-3AKI63ZM-.Q$76QW60H9GN9Z.DE2K/CJ8RJS$S+6TJYJBKBP:3E0JT*I/$KGZI7AU0OIVZJ%VBKLB8DJI7J/VJ0 JYSJEZIK7B*IJOMI$MI1VC3TCCR6HCRF46NY4LGN+KRA+PE$85FQ/84HHP$AA29D KE5.B7KE0.FBK2ZYSX7E73EEJRN31.:5 VJ5OJSFU9BNA5W+FVUKW%LHPIGP1S:LY*TMMV53RKVN9KKN*H8Y4-O5XS3XUVJZMVOS+*K/LN$VMA JQ.VO1WU412AURD55BR8400GPJ2".FromBase45();
+                Assert.True(false);
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.Contains("String length is not correct.", ex.Message);
+            }
+        }
+        [Fact]
+        public void WrongCharacter()
+        {
+            try
+            {
+                "a6BFOXNQG2OAJVUN-8R:*L546/XKPWOSFB60JO DJS4QPR-%2QT45Y7AVD571VX3 43$R71YL423123LF60H9I%5LKB3DJ1OTM7BZGL%0KSVBGZIUSQT35CL6U+QQHPX*OJ+PVC6-S48346-1-P2MED2/D9KD $T2E22R59/9-3AKI63ZM-.Q$76QW60H9GN9Z.DE2K/CJ8RJS$S+6TJYJBKBP:3E0JT*I/$KGZI7AU0OIVZJ%VBKLB8DJI7J/VJ0 JYSJEZIK7B*IJOMI$MI1VC3TCCR6HCRF46NY4LGN+KRA+PE$85FQ/84HHP$AA29D KE5.B7KE0.FBK2ZYSX7E73EEJRN31.:5 VJ5OJSFU9BNA5W+FVUKW%LHPIGP1S:LY*TMMV53RKVN9KKN*H8Y4-O5XS3XUVJZMVOS+*K/LN$VMA JQ.VO1WU412AURD55BR8400GPJ2".FromBase45();
+                Assert.True(false);
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.Equal("Character a doesn't recognize as valid character.", ex.Message);
+            }
+        }
+
         private void TestPhrase(string value)
         {
             string returnValue = value.ToBase45();
--- Base45/Base45.cs
+++ Base45/Base45.cs
@@ -26,11 +26,11 @@ namespace System.Text
             var bytes = Encoding.UTF8.GetBytes(value);
             for (int i = 0; i < bytes.Length; i += 2)
             {
-                var values = bytes.Skip(i).Take(2);
+                var values = bytes.Skip(i).Take(2).ToList();
                 int? second = null;
-                if (values.Count() > 1)
-                    second = values.Last();
-                var returnedValue = Calculate(values.First(), second);
+                if (values.Count > 1)
+                    second = values[1];
+                var returnedValue = Calculate(values[0], second);
                 stringBuilder.Append($"{Values[returnedValue.Item1]}{Values[returnedValue.Item2]}{(returnedValue.Item3.HasValue ? Values[returnedValue.Item3.Value] : string.Empty)}");
             }
             return stringBuilder.ToString();
@@ -50,15 +50,24 @@ namespace System.Text
         }
         public static string FromBase45(this string value)
         {
+            int remainder = value.Length % 3;
+            if (remainder == 1)
+                throw new ArgumentException($"String length is not correct. A possible length is a multiple of 3 or a multiple of 3 minus 1. The actual length is {value.Length}");
             StringBuilder stringBuilder = new StringBuilder();
-            List<int> bytes = value.ToCharArray().Select(x => ValuesAsString[x]).ToList();
-            for (int i = 0; i < bytes.Count; i += 3)
+            List<int> values = new List<int>();
+            foreach (var x in value.ToCharArray())
             {
-                var values = bytes.Skip(i).Take(3);
-                int? third = null;
-                if (values.Count() > 2)
-                    third = values.Last();
-                var returnedValue = Calculate(values.First(), values.Skip(1).First(), third);
+                if (ValuesAsString.ContainsKey(x))
+                    values.Add(ValuesAsString[x]);
+                else
+                    throw new ArgumentException($"Character {x} doesn't recognize as valid character.");
+            }
+            for (int i = 0; i < values.Count; i += 3)
+            {
+                int? e = null;
+                if (i + 2 < values.Count)
+                    e = values[i + 2];
+                var returnedValue = Calculate(values[i], values[i + 1], e);
                 stringBuilder.Append($"{(returnedValue.Item2.HasValue ? new string((char)returnedValue.Item2.Value, 1) : string.Empty)}{(char)returnedValue.Item1}");
             }
             return stringBuilder.ToString();
